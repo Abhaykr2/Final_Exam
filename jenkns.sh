8584785c2e954bcd995e2460012bc893a6f34d05
Abhaykr2 <abhayrathg@gmail.com>
2025-05-27 02:21:22 +0530

    Update jenkns.sh

--- jenkns.sh
+++ jenkns.sh
@@ -4,7 +4,18 @@
 	-> then launch instenc -> Now login to putty
 1.nano Jenkins.sh
 	#!/bin/bash
-		now paste script
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install fontconfig git default-jre -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+systemctl is-active jenkins
 2. chmod u+x Jenkins.sh
 3. ./Jenkins.sh
 4.now copy public ip and run on web with port nuber 8080
